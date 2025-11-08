8c24c2c53a0a546f1ae36138a61d63ade30c67cb
zainshahzadCSUG <zain.shahzad@csuglobal.edu>
2025-11-08 12:37:46 -0500

    Add files via upload

---
+++ CSC320_CT1_ZainS.java
@@ -0,0 +1,19 @@
+public class CSC320_CT1_ZainS {
+    public static void main(String[] args) {
+
+        String firstName = "Clark"; // I chose Clark Kent "Superman" as my fictional character
+        String lastName = "Kent";
+
+        // this is his address according to the comics 
+        String streetAddress = "344, Clinton Street, Apt. 3-B";
+        String city = "Metropolis";
+        String zipCode = "10021";
+
+        // prints out the above data for the user to see
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
